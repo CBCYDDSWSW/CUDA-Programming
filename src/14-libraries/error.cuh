f995bd640871e82c637eae7260ccbef55f9f62f4
CBCYDDSWSW <1296718017@qq.com>
2024-11-08 17:24:52 +0800

    重构了CHECK函数

--- src/14-libraries/error.cuh
+++ src/14-libraries/error.cuh
@@ -1,18 +1,35 @@
+// #pragma once
+// #include <stdio.h>
+
+// #define CHECK(call)                                   \
+// {                                                     \
+//     const cudaError_t error_code = call;              \
+//     if (error_code != cudaSuccess)                    \
+//     {                                                 \
+//         printf("CUDA Error:\n");                      \
+//         printf("    File:       %s\n", __FILE__);     \
+//         printf("    Line:       %d\n", __LINE__);     \
+//         printf("    Error code: %d\n", error_code);   \
+//         printf("    Error text: %s\n",                \
+//             cudaGetErrorString(error_code));          \
+//         exit(1);                                      \
+//     }                                                 \
+// }
+
 #pragma once
-#include <stdio.h>
+#include<stdio.h>
 
 #define CHECK(call)                                   \
 {                                                     \
     const cudaError_t error_code = call;              \
-    if (error_code != cudaSuccess)                    \
-    {                                                 \
+    if(error_code != cudaSuccess)                     \
+    {                                                 \ 
         printf("CUDA Error:\n");                      \
-        printf("    File:       %s\n", __FILE__);     \
-        printf("    Line:       %d\n", __LINE__);     \
-        printf("    Error code: %d\n", error_code);   \
-        printf("    Error text: %s\n",                \
-            cudaGetErrorString(error_code));          \
+        printf("File:%s\n", __FILE__);     \
+        printf("Line:%d\n", __LINE__);     \
+        printf("Error code:%d\n", error_code);   \
+        printf("Error text:%s\n",                \
+        cudaGetErrorString(error_code));          \
         exit(1);                                      \
     }                                                 \
 }
-
